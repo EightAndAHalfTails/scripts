ccf9e77b100461fb835509e62e19625466e05b7b
EightAndAHalfTails <jakebhumphrey@gmail.com>
2013-04-24 09:26:46 +0100

    first commit

---
+++ link_anime.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+def unbracket(string, left='[', right=']'):
+    result = ""
+    bracket_depth = 0
+    for char in string:
+        if char == left:
+            bracket_depth += 1
+        if bracket_depth == 0:
+            result += char
+        if char == right:
+            bracket_depth -= 1
+    return result
+
+def niceify(string):
+    result = unbracket(string)
+    result = unbracket(result, left='(', right=')')
+    result = result.replace('.mkv','')
+    result = result.replace('_',' ')
+    result = result.replace('.',' ')
+    result = result.lstrip()
+    result = result.rstrip()
+    return result
+
+if __name__=="__main__":
+    import argparse, os
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target")
+    args = parser.parse_args()
+    #print(args.target)
+
+    for filename in os.listdir(args.target):
+        link = niceify(filename)
+        target=args.target + '/' + filename
+        #print(args.target + filename)
+        #print(link)
+        os.symlink(target, link)
---
+++ mkv2gif.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+"""Extract animated gifs from mkv files
+
+Usage:
+  mkv2gif.py [options] <input> <output>
+  mkv2gif.py (-h | --help)
+  mkv2gif.py --version
+
+Options:
+  -h --help            Show this screen.
+  --version            Show version.
+  --start=<pos>        Start position in HH:MM:SS format Default is 00:00:00
+  --length=<len>       Length in seconds. Omit to keep going until the end.
+  --resolution=<res>   Output resolution. Default is 640x360
+  --fuzz=<fuz%>        Fuzz factor as percentage
+  --subs=<track>       Subtitle Track. Omit for no subs
+
+"""
+from docopt import docopt
+import os
+import subprocess
+from shlex import quote
+from tempfile import mkdtemp
+from shutil import rmtree
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='mkv2gif 0.1')
+    print(arguments)
+    
+    tmpdir = mkdtemp()
+    subs = os.path.join(tmpdir, "subs.ass")
+    mp4 = os.path.join(tmpdir, "tmp.mp4")
+    frames = os.path.join(tmpdir, arguments['<output>']+"%05d.gif")
+    
+    length = ''
+    if arguments.get('--length') is not None:
+        length = '-t ' + arguments.get('--length')
+
+    start = ''
+    if arguments.get('--start') is not None:
+        start = '-ss ' + arguments.get('--start')
+
+    res = '-s 640x360'
+    if arguments.get('--resolution') is not None:
+        res = '-s ' + arguments.get('--resolution')
+            
+    if(arguments['--subs']):
+        command = "mkvextract tracks {} {}:{}".format(
+            quote(arguments['<input>']),
+            arguments['--subs'],
+            subs)
+        print("Extracting subs...")
+        print('>'+command)
+        subprocess.call(command, shell=True)
+
+        command = "ffmpeg {} -i {} -strict -2 -sn -vcodec libx264 {} -vf ass={} {}".format(
+            length,
+            quote(arguments['<input>']),
+            start,
+            subs,
+            mp4)
+        print('>'+command)
+        subprocess.call(command, shell=True)
+
+        command = "ffmpeg -i {} {} {}".format(
+            mp4,
+            res,
+            frames)
+        print('>'+command)
+        subprocess.call(command, shell=True)
+
+    else:
+        command = "ffmpeg {} {} -i {} {} {}".format(
+            start,
+            length,
+            quote(arguments['<input>']),
+            res,
+            frames)
+        print('>'+command)
+        subprocess.call(command, shell=True)
+
+    fuzz_factor="-fuzz "+arguments['--fuzz']
+    if fuzz_factor is None:
+        fuzz_factor=''
+    command = "convert -delay 5 -loop 0 {} -layers optimize-transparency {} {}.gif".format(
+        fuzz_factor,
+        os.path.join(tmpdir, arguments['<output>']+"*.gif"),
+        arguments['<output>'])
+    print('>'+command)
+    subprocess.call(command, shell=True)
+
+    rmtree(tmpdir)
