4268c5fead5cf3445f4f6c1a1742401eec7e47f3
EightAndAHalfTails <jakebhumphrey@gmail.com>
2013-05-24 15:32:40 +0100

    Sorts on episode order instead of alphabetical order

--- move_with_dest_names.py
+++ move_with_dest_names.py
@@ -3,9 +3,9 @@
 """Move all contents from one directory to another, keeping the destination names. the two directories must have an equal number of files
 
 Usage:
-  stripify.py [options] <source> <destination>
-  stripify.py (-h | --help)
-  stripify.py --version
+  move_with_dest_names.py [options] <source> <destination>
+  move_with_dest_names.py (-h | --help)
+  move_with_dest_names.py --version
 
 Options:
   -h --help            Show this screen.
@@ -17,12 +17,20 @@ Options:
 import os
 import subprocess
 from docopt import docopt
+from link_anime import niceify
+
+def episodeNumber(title):
+    ep_no = niceify(title).split(' ')[-1].split('v')[0]
+    return int(ep_no)
+
+def episodeOrder(titles):
+    return sorted(titles, key=episodeNumber)
 
 def move(source, dest):
     subprocess.call("mv \"{}\" \"{}\"".format(source, dest), shell=True)
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='stripify 0.1')
+    arguments = docopt(__doc__, version='move_with_dest_names 0.1')
     verbose = True if arguments["-v"] or arguments["-n"] else False
 
     dry_run = True if arguments["-n"] else False
@@ -31,17 +39,25 @@ if __name__ == '__main__':
     destdir = arguments["<destination>"]
 
     source_files = os.listdir(sourcedir)
+    if "titles.txt" in source_files:
+        source_files.remove("titles.txt")
     source_files.sort()
 
     dest_files = os.listdir(destdir)
+    #dest_files = episodeOrder(dest_files)
     dest_files.sort()
 
     if len(source_files) is not len(dest_files):
+        print(len(source_files), len(dest_files))
         raise Exception("directories are not equal")
 
     for i in range(len(source_files)):
         source = os.path.join(sourcedir, source_files[i])
+        source = source.replace("`", "\`")
+        #source = source.replace("!", "\!")
         dest = os.path.join(destdir, dest_files[i])
+        dest = dest.replace("`", "\'")
+        #dest = dest.replace("!", "\!")
         if verbose:
             print("{} -> {}".format(source, dest))
         if not dry_run:
